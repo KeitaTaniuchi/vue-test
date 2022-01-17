6bce20df9e13b9aee7cccd95f9d91208a76914d6
k.taniuchi <barbeekonta1995@gmail.com>
2022-01-17 21:27:27 +0900

    dataオプション学習用コード作成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,34 @@
+
 <template>
-  <div class="home">
-    
+  <div class="about">
+    <p>{{ number }}</p>
+    <!-- 123 -->
+    <p>{{ text }}</p>
+    <!-- Hello -->
+    <p>{{ bool }}</p>
+    <!-- true -->
+    <p>{{ arr }}</p>
+    <!-- [ "a", "b", "c" ] -->
+    <p>{{ arr[0] }}</p>
+    <!-- a -->
+    <p>{{ myFruits }}</p>
+    <!-- [ { "name": "オレンジ", "price": 100 }, { "name": "レモン", "price": 200 }] -->
   </div>
 </template>
-
 <script>
 export default {
-  
-}
+  data() {
+    return {
+      number: 123,
+      text: "Hello",
+      bool: true,
+      arr: ["a", "b", "c"],
+      myFruits: [
+        { name: "オレンジ", price: 100 },
+        { name: "レモン", price: 200 },
+        { name: "バナナ", price: 300 },
+      ],
+    };
+  },
+};
 </script>
