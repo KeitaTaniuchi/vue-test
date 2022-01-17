2e3369a88f351c7251b19cce9c0da9b42477f728
k.taniuchi <barbeekonta1995@gmail.com>
2022-01-17 22:06:50 +0900

    methodsとcomputedの使い分け学習用コード作成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,29 +1,47 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-    <ol class="use-computed">
-      <li>{{ computedNumber }}</li>
-      <li>{{ computedNumber }}</li>
-      <li>{{ computedNumber }}</li>
-    </ol>
-    <ol class="use-methods">
-      <li>{{ methodsNumber() }}</li>
-      <li>{{ methodsNumber() }}</li>
-      <li>{{ methodsNumber() }}</li>
-    </ol>
+  <div>
+    <ul v-for="(item, key) in items" :key="key">
+      <li>
+        <span>{{ item.name }}</span
+        >&nbsp;|&nbsp; <span>{{ item.price }}円</span>&nbsp;*&nbsp;
+        <span><input type="number" v-model="item.quantity" />個</span
+        >&nbsp;=&nbsp;
+        <span>{{ item.price * item.quantity }}円</span>
+      </li>
+    </ul>
+    <div>合計金額：{{ totalPriceWithTax }}円(税込)</div>
+    <button @click="doBuy">購入する</button>
   </div>
 </template>
     
 <script>
 export default {
-  computed: {
-    computedNumber: function () {
-      return Math.random(); //0〜1のランダムな数字を生成
-    },
+  data() {
+    return {
+      items: [
+        { name: "レモン", price: 100, quantity: 2 },
+        { name: "りんご", price: 200, quantity: 1 },
+        { name: "メロン", price: 800, quantity: 1 },
+      ],
+    };
   },
   methods: {
-    methodsNumber: function () {
-      return Math.random(); //0〜1のランダムな数字を生成
+    doBuy: function () {
+      window.alert(
+        this.totalPriceWithTax + "円のお買い上げです。ありがとうございました。"
+      );
+    },
+  },
+  computed: {
+    // 合計金額
+    totalPrice: function () {
+      return this.items.reduce(function (sum, item) {
+        return sum + item.price * item.quantity;
+      }, 0);
+    },
+    // 合計金額(税込)
+    totalPriceWithTax: function () {
+      return Math.floor(this.totalPrice * 1.08);
     },
   },
 };
