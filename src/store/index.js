7e971b655c6e3607fd5c2a449a96a38bb1c6964c
k.taniuchi <barbeekonta1995@gmail.com>
2022-01-18 21:04:58 +0900

    Vuex学習のための雛形作成

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,21 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+import counter from "./modules/counter";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
+  strict: process.env.NODE_ENV !== "production",
   state: {
+
   },
   mutations: {
+
   },
   actions: {
+    
   },
   modules: {
-  }
-})
+    counter,
+  },
+});
